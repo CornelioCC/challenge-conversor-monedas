156d7eaf4b26d64e36a756ca84a6d6247fa520fd
CornelioCC <canocastrocornelio@gmail.com>
2025-05-12 17:22:27 -0600

    Crear clase Principal y solicitud de datos a la API

---
+++ src/Principal.java
@@ -0,0 +1,46 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Conversor.exhibirMenu();
+        Scanner scan = new Scanner(System.in);
+        int seleccion = scan.nextInt();
+
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+        String moneda = "";
+        String direccion = "https://v6.exchangerate-api.com/v6/45e0bffdd499c250b1c92642/latest/USD" + moneda;
+
+        try{
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(direccion)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+            System.out.println(json.replace("\"", ""));
+
+
+        }catch(IllegalArgumentException e){
+            System.out.println("Error en la URI, verifique la dirección.");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
+
